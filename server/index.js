713a14c64616efd915aad077a956fe032c7629ba
BrandonCorn <corn_brandon@columbusstate.edu>
2021-02-22 17:31:24 -0500

    add concurrently to launch server and react app to one domain

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ const config = require('./config/keys');
 app.use(bodyParser.json()); 
 app.use(require('./routes')); 
 
+app.get('/', (req, res)=> {
+    return res.status(200).send({data: 'we made it'}); 
+})
+
 app.listen(config.PORT, () => {
     console.log(`Server started on port ${config.PORT}`); 
 })
