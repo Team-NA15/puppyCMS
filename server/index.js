1cbe6ea30532eeb3f26dff47d5d0a537a695e00e
BrandonCorn <corn_brandon@columbusstate.edu>
2021-02-22 22:01:42 -0500

    some changes

--- server/index.js
+++ server/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express(); 
 const bodyParser = require('body-parser'); 
 const config = require('./config/keys'); 
+const path = require('path'); 
 
 
 app.use(bodyParser.json()); 
 app.use(require('./routes')); 
 
-app.get('/', (req, res)=> {
-    return res.status(200).send({data: 'we made it'}); 
-})
+app.use(express.static(path.join(__dirname,'public'))); 
+app.use(express.static(path.resolve(__dirname, 'client/build'))); 
 
 app.listen(config.PORT, () => {
     console.log(`Server started on port ${config.PORT}`); 
