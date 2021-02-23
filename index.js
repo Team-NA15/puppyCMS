79addede980fbaaf411f24dff7bc049850bc67d3
BrandonCorn <corn_brandon@columbusstate.edu>
2021-02-22 21:15:47 -0500

    join path instead of resolve path

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const app = express();
 const bodyParser = require('body-parser'); 
 const config = require('./config/keys'); 
 const path = require('path'); 
+const cors = require('cors'); 
 
 
 app.use(bodyParser.json()); 
-app.use(require('./routes')); 
+app.use(bodyParser.urlencoded({extended: false})); 
+app.use(cors()); 
 
 app.use(express.static(path.join(__dirname,'public'))); 
-app.use(express.static(path.resolve(__dirname, './client/build'))); 
+app.use(express.static(path.join(__dirname, './client/build'))); 
+
+
+app.use(require('./routes')); 
 
 
 app.listen(config.PORT, () => {
