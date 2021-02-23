6ba7c2d5c63fd200f8839ff81b1ef6b7194b3f05
BrandonCorn <corn_brandon@columbusstate.edu>
2021-02-22 21:02:47 -0500

    added proxy to clients package.json, create post build for heroku to install node modules for react app

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const path = require('path');
 app.use(bodyParser.json()); 
 app.use(require('./routes')); 
 
-// app.use(express.static(path.join(__dirname, 'client'))); 
+app.use(express.static(path.join(__dirname,'public'))); 
+app.use(express.static(path.resolve(__dirname, './client/build'))); 
 
 
 app.listen(config.PORT, () => {
