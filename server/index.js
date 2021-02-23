5a76b68dd169fd8731e83648f5601140b08fab4c
BrandonCorn <corn_brandon@columbusstate.edu>
2021-02-22 22:17:36 -0500

    add paths to react views

--- server/index.js
+++ server/index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express(); 
 const bodyParser = require('body-parser'); 
 const config = require('./config/keys'); 
-
+const path = require('path'); 
+const cors = require('cors'); 
 
 app.use(bodyParser.json()); 
-app.use(require('./routes')); 
+app.use(bodyParser.urlencoded({extended: false})); 
+app.use(cors());  
 
+app.use(express.static(path.join(__dirname,'client/build'))); 
+app.use(express.static(path.resolve(__dirname, 'public')));
 
+app.use(require('./routes'));
 
 app.listen(config.PORT, () => {
     console.log(`Server started on port ${config.PORT}`); 
-})
+}); 
