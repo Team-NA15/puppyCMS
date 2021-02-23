58f5d4702885e8125941b43137f6875e5420b4bc
BrandonCorn <corn_brandon@columbusstate.edu>
2021-02-22 19:37:34 -0500

    remove test route

--- server/index.js
+++ server/index.js
@@ -7,10 +7,6 @@ const config = require('./config/keys');
 app.use(bodyParser.json()); 
 app.use(require('./routes')); 
 
-app.get('/', (req, res)=> {
-    return res.status(200).send({data: 'we made it'}); 
-})
-
 app.listen(config.PORT, () => {
     console.log(`Server started on port ${config.PORT}`); 
 })
