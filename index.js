0bc37a5e713a4684f8ab98ed22e92015643a2190
BrandonCorn <corn_brandon@columbusstate.edu>
2021-02-22 21:23:09 -0500

    hardcode express port, add catch to default to home page when request made is not understood

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const config = require('./config/keys'); 
 const path = require('path'); 
 const cors = require('cors'); 
+const { resolve } = require('path');
 
 
 app.use(bodyParser.json()); 
@@ -13,10 +14,13 @@ app.use(cors());
 app.use(express.static(path.join(__dirname,'public'))); 
 app.use(express.static(path.join(__dirname, './client/build'))); 
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build')); 
+})
 
 app.use(require('./routes')); 
 
 
-app.listen(config.PORT, () => {
+app.listen(5000, () => {
     console.log(`Server started on port ${config.PORT}`); 
 })
