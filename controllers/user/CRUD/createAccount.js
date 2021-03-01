370d3c0991aada5a66f58ae338a9c2e207860ef0
U-DESKTOP-TM45V7L\corn_ <corn_brandon@columbusstate.edu>
2021-03-01 14:56:42 -0500

    add comments for easier reading

--- controllers/user/CRUD/createAccount.js
+++ controllers/user/CRUD/createAccount.js
@@ -2,19 +2,6 @@ const bcrypt = require('bcrypt');
 const config = require('../../../config/keys'); 
 const Users = require('../../../db/models').Users; 
 
-
-const encryptPassword = password => {
-    return new Promise ((resolve, reject) => {
-        bcrypt.hash(password, config.SALT)
-        .then( hash => resolve(hash))
-        .catch( err => reject(err))
-    })
-    .catch( err => {
-        console.log(err); 
-        return err; 
-    })
-}
-
 const createAccount = async user => {
     const [newUser, created] = await Users.findOrCreate(user)
     .catch(err => Promise.reject(err)); 
@@ -23,6 +10,5 @@ const createAccount = async user => {
 }
 
 module.exports = {
-    encryptPassword,
     createAccount,
 }
--- routes/users/post/create-account.js
+++ routes/users/post/create-account.js
@@ -1,19 +1,24 @@
 const Users = require('../../../db/models').Users; 
 const {createAccount} = require('../../../controllers/user/CRUD/createAccount'); 
-
+/**
+ * Request Enpoint for User Account Creation
+ * @param {*} req 
+ * @param {*} res 
+ */
 module.exports = async (req,res) => {
     //below is an example of what creating a user will look like, will be replaced in later iteration
+    if (req.body.role === 1) return res.status(404).send({error: 'Not allowed to create super users'}); 
     const user = {
         where: {
-            email: 'donjuan@gmail.com'
+            email: req.body.email
         }, 
         defaults: {
-            password: 'password', 
-            first_name: 'Don', 
-            last_name: 'Henley', 
-            address: 'New York', 
-            phone_number: '958-222-4938', 
-            role: 2, 
+            password: req.body.password, 
+            first_name: req.body.first_name, 
+            last_name: req.body.last_name, 
+            address: req.body.address, 
+            phone_number: req.body.phone_number, 
+            role: req.body.role, 
         }
     }
     const newUser = await createAccount(user)
