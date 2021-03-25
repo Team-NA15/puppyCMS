4b4cb627fce8b52f35853629aa931d91b037497f
BrandonCorn <corn_brandon@columbusstate.edu>
2021-03-25 18:53:43 -0400

    use one try catch block for new appointment

--- routes/appointments/post/new-appointment.js
+++ routes/appointments/post/new-appointment.js
@@ -4,26 +4,26 @@ const { findDogByOwner } = require('../../../controllers/dogs/CRUD/getDog');
 
 module.exports = async (req, res) => {
     let appt, dog; 
-    if(req.query.newDog == 'true'){
-        try{
+    try{
+        if(req.query.newDog == 'true'){
             req.body.new_dog = true; 
             appt = await newAppointment(req.body); 
             return res.status(201).send(); 
-        } 
-        catch(err){
-            return res.status(400).send({name: err.name, message: err.message})
         }
-    }
-    else{
-        try{
+        else{
             req.body.new_dog = false; 
             [dog, appt] = await Promise.all([findDogByOwner(req.body.dog_name, req.body.owner_first_name, req.body.owner_last_name),
                 newAppointment(req.body)]); 
             dog.addAppointment(appt.id);  
             return res.status(201).send();
         }
-        catch(err){
-            return res.status(400).send({name: err.name, message: err.message}); 
-        } 
+    }
+    catch(err){
+        return res.status(400).send({name: err.name, message: err.message}); 
     }
 }
+
+
+ 
+
+
