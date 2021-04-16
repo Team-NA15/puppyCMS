34f021d94ba1350fb44bc82911cde326e8ee1456
BrandonCorn <corn_brandon@columbusstate.edu>
2021-04-16 12:23:03 -0400

    table showing data for dogs appointment

--- client/src/components/AppointmentBlock/appointmentModal.jsx
+++ client/src/components/AppointmentBlock/appointmentModal.jsx
@@ -1,14 +1,25 @@
 import React, { useEffect, useState } from 'react'; 
-import { Modal, Row, Col } from 'react-bootstrap'; 
+import { Modal, Row, Col, Button } from 'react-bootstrap'; 
+import AppointmentTable from './appointmentTable'; 
 
 const AppointmentModal = props => {
+    const [update, setUpdate] = useState(false); 
+
+    const handleUpdate = () => setUpdate(!update); 
     //TODO fix modal shifting background to the left due to scroll bar
     
     return (
-        <Modal size = 'lg' show = {props.show} onHide = {props.handleShowModal}> 
-            <Modal.Header> 
-                {props.dog_name}
+        <Modal size = 'xl' show = {props.show} onHide = {props.handleShowModal} centered = {true}> 
+            <Modal.Header closeButton>
+                <Modal.Title> {props.dog_name} </Modal.Title>
             </Modal.Header>
+            <Modal.Body> 
+                <AppointmentTable {...props} />  
+            </Modal.Body>
+            <Modal.Footer> 
+                <Button onClick = {props.handleShowModal}> Close </Button> 
+                <Button > Update </Button>
+            </Modal.Footer>
         </Modal> 
     )
 }
