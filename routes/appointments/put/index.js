7ff588fb4edf38aebbed91b1f6dac013a5cabf99
Alyssia21 <mahone_alyssia@columbusstate.edu>
2021-04-20 19:05:20 -0400

    implementd dog checkout endpoint

--- routes/appointments/put/index.js
+++ routes/appointments/put/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const auth = require('../../../middleware/auth');  
 
 router.put('/check-in-appointment', auth, require('./check-in-appt')); 
+router.put('/check-out-appointment', auth, require('./check-out-appt')); 
 
 module.exports = router; 
