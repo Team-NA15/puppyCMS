2a591d5a236f2aa20eca2c1fd72498fb3714c468
BrandonCorn <corn_brandon@columbusstate.edu>
2021-04-28 18:17:03 -0400

    add hover to dog info on appointment history search, remove check-in/check-out button when dog is checked out

--- client/src/components/Dogs/dogInfoCard.jsx
+++ client/src/components/Dogs/dogInfoCard.jsx
@@ -1,13 +1,14 @@
 import React from 'react'; 
 import { Card, Row, Col, Table, } from 'react-bootstrap'; 
+import './dogInfo.scss'; 
 
 const DogInfoCard = ({clickAction, ...props}) => {
     const {dog_name, owner_first_name, owner_last_name, breed, color, gender } = props; 
 
     return (
         <div> 
-            <a style = {{cursor: 'pointer', }} onClick = {clickAction ? clickAction : () => {}}> 
-                <Card border = 'light'> 
+            <a hover style = {{cursor: 'pointer'}} onClick = {clickAction ? clickAction : () => {}}> 
+                <Card className = 'dog-info-card' border = 'light'> 
                     <Card.Header> <b> {dog_name} </b> </Card.Header>
                     <Card.Body> 
                         <Table bordered> 
